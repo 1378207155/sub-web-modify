daa105a3f15209e186f37f0d780ce7d321f99d62
1378207155 <146457407+1378207155@users.noreply.github.com>
2026-01-20 09:35:52 +0800

    1

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -782,7 +782,7 @@ export default {
       form: {
         sourceSubUrl: "",
         clientType: "",
-        customBackend: this.getUrlParam() == "" ? "https://url.v1.mk" : this.getUrlParam(),
+        customBackend: this.getUrlParam() == "" ? "https://subapi.coisini.us.kg" : this.getUrlParam(),
         shortType: "https://v1.mk/short",
         remoteConfig: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online.ini",
         excludeRemarks: "",
@@ -1306,6 +1306,7 @@ export default {
   }
 };
 </script>
+
 
 
 
